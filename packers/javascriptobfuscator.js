dd20e484a9ed1934d84926f110b51ffd425aee97
kokhinmaungwin <khinmaungwin675@gmail.com>
2026-01-11 23:47:35 +0630

    Update javascriptobfuscator.js

--- packers/javascriptobfuscator.js
+++ packers/javascriptobfuscator.js
@@ -1,13 +1,18 @@
-export function JSObfuscator(source) {
-  const sandbox = {};
-  const fn = new Function("sandbox", `
-    with(sandbox){
-      ${source}
-      return typeof _0x !== "undefined" ? _0x : null;
-    }
-  `);
-  try { fn(sandbox); } catch {}
+export const JavascriptObfuscator = {
+  detect(source) {
+    return /_0x[a-f0-9]{4,6}/i.test(source);
+  },
 
-  source = source.replace(/_0x[a-f0-9]{4,6}\((0x[a-f0-9]+)\)/gi, m => eval(m));
-  return source;
-}
+  unpack(source) {
+    const sandbox = {};
+    const fn = new Function("sandbox", `
+      with(sandbox){
+        ${source}
+        return typeof _0x !== "undefined" ? _0x : null;
+      }
+    `);
+    try { fn(sandbox); } catch {}
+
+    return source.replace(/_0x[a-f0-9]{4,6}\((0x[a-f0-9]+)\)/gi, m => eval(m));
+  }
+};
