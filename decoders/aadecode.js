3afd2ce27d749496aa6c78ae766c1681db1eae25
kokhinmaungwin <khinmaungwin675@gmail.com>
2026-01-11 17:22:46 +0630

    Update aadecode.js

--- decoders/aadecode.js
+++ decoders/aadecode.js
@@ -1,22 +1,27 @@
 /* AA Decode */
 export const AADecode = {
-  decode(text) {
-    const evalPreamble = "(\uFF9F\u0414\uFF9F) ['_'] ( (\uFF9F\u0414\uFF9F) ['_'] (";
-    const decodePreamble = "( (\uFF9F\u0414\uFF9F) ['_'] (";
-    const evalPostamble = ") (\uFF9F\u0398\uFF9F)) ('_');";
-    const decodePostamble = ") ());";
+    decode: function(text) {
+      const evalPreamble = "(\uFF9F\u0414\uFF9F) ['_'] ( (\uFF9F\u0414\uFF9F) ['_'] (";
+      const decodePreamble = "( (\uFF9F\u0414\uFF9F) ['_'] (";
+      const evalPostamble = ") (\uFF9F\u0398\uFF9F)) ('_');";
+      const decodePostamble = ") ());";
 
-    text = String(text).trim();
-    if (!text) return "";
+      text = text.trim();
 
-    if (text.indexOf(evalPreamble) < 0 || !text.endsWith(evalPostamble)) {
-      throw new Error("Given code is not encoded as aaencode.");
+      if (!text) return "";
+
+      if (text.indexOf(evalPreamble) < 0 || !text.endsWith(evalPostamble)) {
+        throw new Error("Given code is not encoded as aaencode.");
+      }
+
+      const decodingScript = text.replace(evalPreamble, decodePreamble)
+                                 .replace(evalPostamble, decodePostamble);
+
+      // Dummy variables
+      const ﾟ = {};
+      const ω = {};
+      const ﾉ = function() {};
+
+      return eval(decodingScript);
     }
-
-    const decodingScript = text
-      .replace(evalPreamble, decodePreamble)
-      .replace(evalPostamble, decodePostamble);
-
-    return eval(decodingScript);
-  }
-};
+  };
